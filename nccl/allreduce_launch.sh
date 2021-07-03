2081b6d9aa127c694e259dfcb616d5a1435a8dc8
Zhen Zhang <zhangzhen.dev@gmail.com>
2021-07-03 00:31:41 +0000

    add mpirun option

--- nccl/allreduce_launch.sh
+++ nccl/allreduce_launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NP=8
-HOSTS="172.31.6.51:4,172.31.24.30:4"
+NP=16
+HOSTS="172.31.9.0:8,172.31.12.234:8"
 MPI_HOME="/opt/amazon/openmpi"
 TEST_BIN="/home/ec2-user/RandomPieces/nccl/allreduce.bin"
 
@@ -14,12 +14,17 @@ cmd="${MPI_BIN} -np ${NP} \
         -tag-output \
         -bind-to none -map-by slot \
         -x PATH \
+        -x NCCL_ALGO=Ring \
+        -x FI_EFA_USE_DEVICE_RDMA=1 \
+        -x RDMAV_FORK_SAFE=1 \
+        -x FI_PROVIDER=\"efa\" \
         -x NCCL_SOCKET_IFNAME=eth0 \
-        -x NCCL_DEBUG=DEBUG \
+        -x NCCL_DEBUG=INFO \
         -x NCCL_TREE_THRESHOLD=0 \
         -x LD_LIBRARY_PATH=${LD_LIBRARY_PATH} \
         -mca btl ^openib \
+        -mca pml ob1 \
         -mca btl_tcp_if_exclude lo,docker0 \
-        ${TEST_BIN} 4 1000000"
+        ${TEST_BIN} 8 100000000"
 
 eval ${cmd}
